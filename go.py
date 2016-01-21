0711243b1ced465a26ba6f6e8f9a89d328c6103e
Kevin Lacker <lacker@gmail.com>
2016-01-20 17:20:10 -0800

    more vector messing around

--- go.py
+++ go.py
@@ -19,14 +19,42 @@ def features(n):
   return substrings('a{0:b}z'.format(n))
 
 
+'''
+x should be an odd number.
+
+Makes a vector for x-features minus (xprime)-features,
+where xprime has had one step of HOTPO applied.
+If we can find a +/- symmetric function of this vector that is always
+positive, then that's the monovariant we are looking for.
+'''
+def diff(x):
+  xprime = 3 * x + 1
+  while xprime % 2 == 0:
+    xprime /= 2
+
+  # TODO: other logic here
+  
+  
 class Vector:
-  '''Make a vector by counting features.'''
-  def __init__(self, feats):
+  '''
+  For pairs, just treats each pair as a feature -> value entry.
+  '''
+  def __init__(self, pairs):
     # The components maps features to a coefficient.
     self.components = {}
-    for f in feats:
-      self.components[f] = self.components.get(f, 0) + 1
+    for f, v in pairs
+      self.components[f] = self.components.get(f, 0) + v
 
+  def __add__(self, other):
+    return Vector(self.components.items() + other.components.items())
+
+  def scale(self, k):
+    for key, val in self.components.items():
+      self.components[key] = k * val
+
+  def __sub__(self, other):
+    return self + other.scale(-1)
+    
 
 def main():
   print substrings('football')
